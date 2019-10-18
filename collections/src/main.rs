c7b0d0761715e4ed06fe70bfe36ca6ee8a45baee
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2019-10-18 15:46:04 -0600

    Finish up collections

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -219,3 +219,111 @@ for c in "नमस्ते".chars() {
 for b in "नमस्ते".bytes() {
     println!("{}", b);
 }
+
+// Hash Maps
+// Just like vectors, hash maps store their data on the heap.
+// You can create an empty hash map with new and add elements with insert.
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+// Another way of constructing a hash map is by using the collect method on a vector of tuples, where each tuple
+// consists of a key and its value. The collect method gathers data into a number of collection types, including
+// HashMap. For example, if we had the team names and initial scores in two separate vectors, we could use the
+// zip method to create a vector of tuples where “Blue” is paired with 10, and so forth. Then we could use the collect
+// method to turn that vector of tuples into a hash map
+use std::collections::HashMap;
+
+let teams = vec![String::from("Blue"), String::from("Yellow")];
+let initial_scores = vec![10, 50];
+
+let scores: HashMap<_, _> = teams.iter().zip(initial_scores.iter()).collect();
+// The type annotation HashMap<_, _> is needed here because it’s possible to collect into many different data structures
+// and Rust doesn’t know which you want unless you specify. For the parameters for the key and value types, however, we
+// use underscores, and Rust can infer the types that the hash map contains based on the types of the data in the vectors
+
+// For types that implement the Copy trait, like i32, the values are copied into the hash map. For owned values like String,
+// the values will be moved and the hash map will be the owner of those values
+use std::collections::HashMap;
+
+let field_name = String::from("Favorite color");
+let field_value = String::from("Blue");
+
+let mut map = HashMap::new();
+map.insert(field_name, field_value);
+// field_name and field_value are invalid at this point, try using them and compiler will be angry!
+
+// We can get a value out of the hash map by providing its key to the get method
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+let team_name = String::from("Blue");
+let score = scores.get(&team_name)
+
+// We can iterate over each key/value pair in a hash map in a similar manner as we do with vectors, using a for loop:
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Yellow"), 50);
+
+for (key, value) in &scores {
+  println!("{}: {}", key, value)
+}
+
+// If we insert a key and a value into a hash map and then insert that same key with a different value, the value associated with
+// that key will be replaced. Even though the code in Listing 8-24 calls insert twice, the hash map will only contain one
+// key/value pair because we’re inserting the value for the Blue team’s key both times.
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.insert(String::from("Blue"), 25);
+
+println!("{:?}", scores); // {"Blue": 25} -> second insert overwrote the first
+
+// Only Inserting a Value If the Key Has No Value
+use std::collections::HashMap;
+
+let mut scores = HashMap::new();
+
+scores.insert(String::from("Blue"), 10);
+scores.entry(String::from("Yellow")).or_insert(50);
+scores.entry(String::from("Blue")).or_insert(50);
+
+println!("{:?}", scores); // {"Yellow": 50, "Blue": 10}
+
+// Updating a value based on the old value
+use std::collections::HashMap;
+
+let text = "hello world wonderful world";
+
+let mut map = HashMap::new();
+
+for word in text.split_whitespace() {
+    let count = map.entry(word).or_insert(0);
+    *count += 1;
+}
+
+println!("{:?}", map);
+// This code will print {"world": 2, "hello": 1, "wonderful": 1}. The or_insert method actually returns a mutable
+// reference (&mut V) to the value for this key. Here we store that mutable reference in the count variable, so in
+// order to assign to that value, we must first dereference count using the asterisk (*). The mutable reference goes
+// out of scope at the end of the for loop, so all of these changes are safe and allowed by the borrowing rules.
+
+// By default, HashMap uses a “cryptographically strong”1 hashing function that can provide resistance to Denial of Service
+// (DoS) attacks. This is not the fastest hashing algorithm available, but the trade-off for better security that comes with
+// the drop in performance is worth it. If you profile your code and find that the default hash function is too slow for your
+// purposes, you can switch to another function by specifying a different hasher. A hasher is a type that implements the
+// BuildHasher trait. We’ll talk about traits and how to implement them in Chapter 10. You don’t necessarily have to
+// implement your own hasher from scratch; crates.io has libraries shared by other Rust users that provide hashers implementing
+// many common hashing algorithms.
